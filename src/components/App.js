2a17cf61aa66820bca1f88bb1189fb0a89637839
Vaibhav Bisht <92950515+beastvaibhv@users.noreply.github.com>
2023-12-24 06:00:42 +0000

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,32 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React ,{useState} from "react";
+
+const App =()=>{
+    const [item, setItem] = useState("");
+    const [shoppingList, setShoppingList] = useState([]);
+    
+    const handleClearList=()=>{
+        setShoppingList([]);
+    }
+    const handleAddItem=()=>{
+
+    }
+    const handleLogin=()=>{
+
+    }
+    const handleSignout=()=>{
+
+    }
+    const handleRemoveItem =()=>{
+        
+    }
+
+    return(
+      <div>
+        <button id="login-btn" onClick={handleLogin}>Login</button>
+        <button id="signout" onClick={handleSignout}>SignOut</button>
+        <div><input id="shopping-input" type="text" onChange={(e)=>{setItem(e.target.value)}} value={item}/> <button id="add-btn" onClick={handleAddItem}>Add</button></div>
+        <button id="clearlist" onClick={handleClearList}>Clear List</button>
+      </div>
+    )
+}
 
