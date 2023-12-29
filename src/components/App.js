0566114a49caafa8d175d8d3cbaa5cb186007f9a
Vaibhav Bisht <92950515+beastvaibhv@users.noreply.github.com>
2023-12-29 20:46:55 +0000

    second save

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import UserContext from "./UserContext";
 
 
@@ -6,50 +6,75 @@ const App = () => {
   const [item, setItem] = useState("");
   const [shoppingList, setShoppingList] = useState([]);
   const [isSignIn, setIsSignIn] = useState(false);
+
+  const handleText = (e) => {
+    let t = e.target.value;
+    setItem(t);
+  }
   const data = useContext(UserContext);
+  console.log(data);
 
   const handleClearList = () => {
     setShoppingList([]);
-  }
-  const handleAddItem = () => {
-    setShoppingList((previousList) => [...previousList, item])
-  }
-  const handleLogin = () => {
-    setIsSignIn(true);
-  }
-  const handleSignout = () => {
-    setIsSignIn(false);
-  }
-  const handleRemoveItem = (value) => {
-    const itemName = value.split('-')[1]; 
-    
-    let newList = shoppingList.filter((item)=>{
-      item.id.split('-')[1] !== itemName;
-     })
-     setShoppingList([newList]);
-  }
+    console.log("clear list")
+  };
 
+  const handleAddItem = () => {
+    if (item.trim() !== "") {
+      setShoppingList((prevItems) => [...prevItems, item])
+      
+    }
+    setItem("")
+  };
+  console.log(shoppingList);
+
+
+  const handleLogin = () => {
+
+  };
+  const handleSignout = () => {
+
+  };
+
+const handleRemove=()=>{
+  
+}
   return (
     <UserContext.Provider value={{ isAuthenticated: isSignIn }}>
       <div>
         <div>
           <h2>use context hook</h2>
           <h1>Hello {data.currentUser}</h1>
-          <ul>{shoppingList.map((item) => {
-            <li id={"item-" + item}>{item} <button onClick={handleRemoveItem(id)} id={"remove-"+item}>Remove</button></li>
 
-          })}</ul>
+
+          <ul style={{ listStyleType: "none" }}>
+
+            {shoppingList.map((item) => (<li id={"item-" + item} key={item}> {item}  {"  "}   <button onClick={handleRemove} id={"remove-" + item}>Remove</button> </li>))}
+          </ul>
 
           <h2>Hi {data.currentUser} again, are you in Agra?</h2>
           <h2>use Reducer Hook</h2>
-          <h6>Current user: {data.currentUser}, isAuthenticated: {isAuthenticated ? "Yes" : "No"}</h6>
+          <h6>Current user: {data.currentUser}, isAuthenticated: {data.isAuthenticated ? "Yes" : "No"}</h6>
         </div>
         <button id="login-btn" onClick={handleLogin}>Login</button>
         <button id="signout" onClick={handleSignout}>SignOut</button>
-        <div><input id="shopping-input" type="text" onChange={(e) => { setItem(e.target.value) }} value={item} /> <button id="add-btn" onClick={handleAddItem}>Add</button></div>
+        <div>
+          <input 
+          id="shopping-input" 
+          type="text" 
+          onChange={handleText} 
+          value={item} />
+
+          <button 
+          id="add-btn" 
+          onClick={handleAddItem}>
+            Add
+            </button>
+        </div>
         <button id="clearlist" onClick={handleClearList}>Clear List</button>
       </div>
     </UserContext.Provider>
   )
 }
 
+export default App;
